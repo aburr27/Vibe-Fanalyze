72b92daefbbe3df7bcb2c84f3ef6c38f9732461b
Arebria Burr <47797027+aburr27@users.noreply.github.com>
2025-06-04 22:59:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
-from backend.routers import stats, players, teams, games, fantasy  # Make sure this matches your folder structure
-from backend.routes import fantasy
+from backend.routes import stats, players, teams, games, fantasy  # Make sure this matches your folder structure
 
 # Create FastAPI instance
 app = FastAPI(
