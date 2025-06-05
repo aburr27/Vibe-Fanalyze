b4168b94da0076b147db168d73edea7e70a8c471
Arebria Burr <47797027+aburr27@users.noreply.github.com>
2025-06-04 21:49:24 -0500

    Update mysql_connector.py

--- db/mysql_connector.py
+++ db/mysql_connector.py
@@ -3,8 +3,11 @@ from backend.config import settings
 
 def get_mysql_connection():
     return pymysql.connect(
-        host=settings.MYSQL_URL,
-        user='root',
-        password='your_password',
-        db='sportsdb'
+        host=settings.MYSQL_HOST,
+        user=settings.MYSQL_USER,
+        password=settings.MYSQL_PASSWORD,
+        database=settings.MYSQL_DB,
+        port=int(settings.MYSQL_PORT),
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
     )
